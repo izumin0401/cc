924a7c9e62b95dea71fc0e4eae614e22d1f6c47a
kohei izumi <unitedbz0401@gmail.com>
2020-03-22 01:36:01 +0900

    first commit

---
+++ lambda.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    const int a = 1;
+    const int b = 2;
+    
+    /*
+     * ■Lambda式について
+     * 
+     * Lambda式の型は必ず「auto」となる。
+     * ⇒コンパイラがユニークな型を持つため、全く同じ記述でも異なる型としてみなされるため。
+     * 
+     * [=]により、Lambda式外で宣言された変数が自動的にコピーされ、ラムダ式内で使用できるようになる。
+     */
+    auto add = [=](int c) { // 「[](int i) -> void」と同様だが、ラムダ式は型推論によりreturnの型を省略できる
+        std::cout << "合計は" << a + b + c << "です。" << std::endl;
+    };
+    
+    add(3);
+}
